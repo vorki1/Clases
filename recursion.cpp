7ca61eef6e99c4aaadf0718cd3afe8fdb1cb5c12
vorki1 <130622594+vorki1@users.noreply.github.com>
2023-11-10 19:11:42 +0000

    Hola

---
+++ recursion.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int transformarBinario(int x)
+{
+    if(x<2)
+    {
+
+    }
+    else if(x % 2==0 || x % 2==1)
+    {
+
+    }
+}
+
+int main()
+{
+    //Escribir un numero decimal a binario
+    transformarBinario(5);
+    return 0;
+}

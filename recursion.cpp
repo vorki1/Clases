08a1d8d8a5cd9c01e3ff76c5785b2f41b6cd84cf
vorki1 <130622594+vorki1@users.noreply.github.com>
2023-11-11 20:48:11 +0000

    Se hizo

--- recursion.cpp
+++ recursion.cpp
@@ -1,25 +1,13 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 using namespace std;
 
-void moverPila(stack<int> pila)
+void moverPila(vector<int>lista,vector<int>aux1,vector<int>aux2)
 {
-    if(!pila.empty())
-    {
-        cout<<pila.top()<<endl;
-        pila.pop();
-        moverPila(pila);
-    }
 }
 
 int main()
 {
-    stack<int> pila,aux1,aux2;
-    pila.push(10);
-    pila.push(7);
-    pila.push(4);
-    pila.push(3);
-    pila.push(1);
-    moverPila(pila);
+    vector<int>lista = {1,3,5,6,7};
     return 0;
 }
